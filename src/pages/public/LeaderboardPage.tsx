18e52012320cc52f2612d6406bd03ef39c875ae8
shariqbabu <36099197+shariqbabu@users.noreply.github.com>
2026-06-03 17:13:15 +0530

    Update LeaderboardPage.tsx

--- src/pages/public/LeaderboardPage.tsx
+++ src/pages/public/LeaderboardPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Layout } from '@/components/common/Layout';
+import Layout from '@/components/common/Layout';
 import { useLeaderboard } from '@/hooks/useMatch';
 import { TrendingUp, User } from 'lucide-react';
 
