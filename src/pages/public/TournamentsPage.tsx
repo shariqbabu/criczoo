0fd608a612c6a8555c7865bd344bf36962826848
shariqbabu <36099197+shariqbabu@users.noreply.github.com>
2026-06-03 17:14:08 +0530

    Update TournamentsPage.tsx

--- src/pages/public/TournamentsPage.tsx
+++ src/pages/public/TournamentsPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Layout } from '@/components/common/Layout';
+import Layout from '@/components/common/Layout';
 import { useTournaments } from '@/hooks/useMatch';
 import { Trophy, Search, Calendar } from 'lucide-react';
 
