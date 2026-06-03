f2e074132587429dc927e4bdf16b74d1dd8361e2
shariqbabu <36099197+shariqbabu@users.noreply.github.com>
2026-06-03 16:48:31 +0530

    Update ScorecardPage.tsx

--- src/pages/public/ScorecardPage.tsx
+++ src/pages/public/ScorecardPage.tsx
@@ -1,5 +1,5 @@
 import { useParams, Link } from 'react-router-dom';
-import { Layout } from '@/components/common/Layout';
+import { Layout } from '@/components/Layout';
 import { useMatch } from '@/hooks/useMatch';
 import { ArrowLeft } from 'lucide-react';
 
