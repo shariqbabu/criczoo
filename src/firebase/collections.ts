844f906b0424f5beb9c75a47c48adaedb90b0ffc
shariqbabu <36099197+shariqbabu@users.noreply.github.com>
2026-06-03 13:04:02 +0530

    Delete src directory

--- src/firebase/collections.ts
+++
@@ -1,8 +0,0 @@
-export const COLLECTIONS = {
-  USERS: 'users',
-  MATCHES: 'matches',
-  TOURNAMENTS: 'tournaments',
-  TEAMS: 'teams',
-  PLAYERS: 'players',
-  COMMENTARY: 'commentary',
-} as const;
